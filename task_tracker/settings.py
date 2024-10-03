f99abc0695916f43cf590d1ebd64c877923f9142
Alexey Kurylev <chepe4pi@gmail.com>
2024-10-03 14:11:27 -0300

    add static

--- task_tracker/settings.py
+++ task_tracker/settings.py
@@ -120,7 +120,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -135,3 +134,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     )
 }
+
+STATIC_URL = '/static/'
+STATIC_ROOT = 'static/'
